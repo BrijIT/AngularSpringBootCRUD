034b9152a768e28b6ae90888263007660f52d396
Brij mohan <mohan.bri47@gmail.com>
2023-07-06 13:30:44 +0530

    Initial Commit

---
+++ src/app/update-employee/update-employee.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, enableProdMode } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-update-employee',
+  templateUrl: './update-employee.component.html',
+  styleUrls: ['./update-employee.component.css']
+})
+export class UpdateEmployeeComponent implements OnInit {
+  id: number;
+  employee: Employee = new Employee();
+  constructor(private employeeService: EmployeeService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.employeeService.getEmployeeById(this.id).subscribe(data => {
+      this.employee = data;
+    }, error => console.log(error));
+  }
+
+  onSubmit() {
+    this.employeeService.updateEmployee(this.id, this.employee).subscribe(data => {
+      this.goToEmployeeList();
+    }, error => console.log(error));
+  }
+
+  goToEmployeeList() {
+    this.router.navigate(['/employee']);
+  }
+
+}
